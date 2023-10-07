97b5c9baef5a7c2f2e8726a3cf530cd40de20108
ranaagouda <ranagouda01@gmail.com>
2023-10-07 13:12:21 +0300

    Solved the validation bug

--- src/validation.js
+++ src/validation.js
@@ -1,7 +1,6 @@
 function validateName() {
   //name
-  console.log("name", name);
-  let name = getElementById("name").value;
+  let name = document.getElementById("name").value;
   if (name.length < 3) {
     alert("Name should be at least 3 characters long.");
     return false;
@@ -10,8 +9,7 @@ function validateName() {
 
 function validateEmail() {
   //email
-  console.log("email", email);
-  let email = getElementById("email").value;
+  let email = document.getElementById("email").value;
   let atpos = email.indexOf("@");
   let dotpos = email.lastIndexOf(".");
   if (atpos < 1 || dotpos - atpos < 2) {
@@ -22,10 +20,9 @@ function validateEmail() {
 
 function validatePassword() {
   //password
-  console.log("password", password);
   var decimal =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-  let password = getElementById("password").value;
+  let password = document.getElementById("password").value;
   if (password.match(decimal)) {
     alert(
       "Password should contain at least one capital letter and one number, and be at least 8 characters long."
